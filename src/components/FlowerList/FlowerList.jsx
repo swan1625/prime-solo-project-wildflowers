4a9660977d90bc81f0469d1305cade602a935d15
Andy Swanson <swan1625@icloud.com>
2022-03-22 00:17:55 -0500

    organize by color dropdown working now

--- src/components/FlowerList/FlowerList.jsx
+++ src/components/FlowerList/FlowerList.jsx
@@ -19,19 +19,39 @@ function flowerList(){
 
 
     const [color, setColor] = useState('');
+    const [filteredList, SetFilteredList] = useState(flowers);
+
+   
+    const filterByColor = (filteredData) => {
+        console.log('this is filter list', filteredList );
+      if (!color){
+        return filteredData;
+      }
+      const filteredColor = filteredData.filter(
+        (flower) => flower.color === color
+      );
+      return filteredColor;
+    };
+
 
     const handleChange = (event) => {
       setColor(event.target.value);
-      dispatch({type: 'SEARCH_COLOR', payload: color})
+      // filterByColor();
+      console.log('test sdffdfglowers', filteredList);
     };
 
     useEffect(() => {
         // on page load, fetch these things
-        dispatch({ type: 'FETCH_FLOWERS' });
-    }, []);
+  
+        // dispatch({ type: 'FETCH_FLOWERS' });
+        let filteredData = filterByColor(flowers);
+        SetFilteredList(filteredData);
+      
+    }, [color]);
 
-console.log(flowers);
+console.log('flowers is', flowers);
 console.log('color is',color);
+
     return (
 <>
 
@@ -47,21 +67,18 @@ console.log('color is',color);
         >
           <MenuItem value={'Red'}>Red Flowers ❤️</MenuItem>
           <MenuItem value={'Blue'}>Blue Flowers 💙 </MenuItem>
-          <MenuItem value={'Pink'}>Pink/Purple Flowers 💜💓</MenuItem>
+          <MenuItem value={'Purple'}>Pink/Purple Flowers 💜💓</MenuItem>
           <MenuItem value={'Orange'}>Orange Flowers 🧡</MenuItem>
           <MenuItem value={'White'}>White Flowers 🤍</MenuItem>
+          <MenuItem value={'Yellow'}>Yellow Flowers 💛</MenuItem>
         </Select>
       </FormControl>
     </Box>
 
 
-
-
-
-
         
         <div>
-        {flowers.map((item, i) => {
+        {filteredList.map((item, i) => {
                 return (
                     <FlowerListItem key={i} item={item} />
                 )
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
+
+  const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+  useEffect(() => {
+    dispatch({ type: 'FETCH_FLOWERS' });
+}, []);
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
