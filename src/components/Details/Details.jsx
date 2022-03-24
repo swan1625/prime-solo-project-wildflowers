cdb46ff24a409927e7711e761300758f5104bd7a
Andy Swanson <swan1625@icloud.com>
2022-03-24 13:03:05 -0500

    styling half done on the details page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,15 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import './Details.css';
 
+import { Box, fontSize } from '@mui/system';
 
-function detailsPage(){
+function detailsPage() {
 
     const selectedFlower = useSelector(store => store.selectedFlower);
 
     const dispatch = useDispatch();
     const history = useHistory();
-
 
     function handleClick() {
         console.log('clicking back to list');
@@ -22,31 +23,28 @@ function detailsPage(){
         history.push('/submit')
     }
 
-
-
     useEffect(() => {
         // 
         dispatch({ type: 'FETCH_SELECT_FLOWER', payload: selectedFlower.id })
     }, []);
 
-    return(
+    return (
+        <div>
+            <div className='details'>
+                <p style={{fontSize: 'xx-large'}}> <b>{selectedFlower.common_name} </b> </p>
+                <img src={selectedFlower.image_path} />
+                <p> <b>Scientific Name: </b> {selectedFlower.scientific_name} </p>
+                <p><b> Color:  </b> {selectedFlower.color} </p>
+                <p> <b>Height:</b> {selectedFlower.flower_size} </p>
+                <p> <b>Genus:</b> {selectedFlower.genus} </p>
+                <p> <b>Life Cycle:</b> {selectedFlower.life_cycle}</p>
+                <p><b>Status:</b> {selectedFlower.type}</p>
+                <button onClick={handleClick}> Back to Complete List </button>
+                <button onClick={handleMatchClick}> Think you have a Match? </button>
 
-        <>
-        <p> <b>{selectedFlower.common_name} </b> </p>
-        <img src={selectedFlower.image_path} />
-        <p> Scientific Name: {selectedFlower.scientific_name} </p>
-        <p> Color: {selectedFlower.color} </p>
-        <p> Height: {selectedFlower.flower_size} </p>
-        <p> Genus: {selectedFlower.genus} </p>
-        <p> Life Cycle: {selectedFlower.life_cycle}</p>
-        <p>Status: {selectedFlower.type}</p>
-        <button onClick = {handleClick}> Back to Complete List </button>
-        <button onClick = {handleMatchClick}> Think you have a Match? </button>
-        
-        </>
+            </div>
+        </div>
     )
-
-
 
 }
 
