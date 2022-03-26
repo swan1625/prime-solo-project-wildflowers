a0e1f783a556bd6bb2e12b417547ce8e407f6327
Andy Swanson <swan1625@icloud.com>
2022-03-26 16:38:10 -0500

    showing picked flower on confirmation page

--- src/components/SubmitConfirmation/SubmitConfirmation.jsx
+++ src/components/SubmitConfirmation/SubmitConfirmation.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-
+import './SubmitConfirmation.css'
 
 
 
 function submitConfirm() {
-
+    const selectedFlower = useSelector(store => store.selectedFlower);
 
     const history = useHistory();
     function handleListClick() {
@@ -21,11 +21,12 @@ function submitConfirm() {
         history.push('/userlist')
     }
 
+    console.log('selected floweradnrea', selectedFlower);
     return (
 
         <>
-        Awesome! You Pic(k)ed a flower!
-
+       <p> Successful Upload!  <br></br> You Pic(k)ed a <b> {selectedFlower.common_name} </b> </p>
+        <img className='andreaFlower' src="https://res.cloudinary.com/dubyogkaz/image/upload/v1648328856/wildflowers/wvam0wnejl4zkuvsq4zw.png"></img>
         <button onClick={handleListClick}> Back to Complete List </button>
         <button onClick={handleBouquetClick}> Check out bouquet </button>
         </>
