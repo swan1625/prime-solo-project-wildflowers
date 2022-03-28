b9af2c99aa028c8c30da1147956293874634b0bf
Andy Swanson <swan1625@icloud.com>
2022-03-27 23:11:15 -0500

    about me page is up as well as nav bar closing on click

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AboutPage.css'
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -6,12 +7,32 @@ import React from 'react';
 // or even care what the redux state is'
 
 function AboutPage() {
-  return (
-    <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
-      </div>
+  return (<>
+  <div style={{ borderStyle: 'dashed', borderColor: '#a5c2af', marginTop: '35px', backgroundColor: '#e2d3e8'}}>
+    <div className="about-container">
+      <h2 className="listInfoHeading"> Technologies Used</h2>
+      <h4 className="listItem" > • ReactJS / ReduxJS / Javascript </h4>
+      <h4 className="listItem" > • Node.js  / Express / PostgreSQL  </h4>
+      <h4 className="listItem" > • MaterialUI / Html / CSS </h4>
+      <h4 className="listItem" > • Multer Image Upload / Cloudinary </h4>
     </div>
+        <div className="about-container">
+        <h2 className="listInfoHeading"> Thank you</h2>
+        <h4 className="listItem" > • Liz Kerber, Matt Black, Kris Szafranski </h4>
+        <h4 className="listItem" > • Prime Digital Academy </h4>
+        <h4 className="listItem" > • Family and Friends </h4>
+        <h4 className="listItem" > • Adams Cohort </h4>
+        <h4 className="listItem" > • www.minnesotawildflowers.info </h4>
+      </div>
+      <div className="about-container">
+        <h2 className="listInfoHeading"> Contact Info </h2>
+        <h4 className="listItem" > <b>Email:</b> andyswanson44@gmail.com </h4>
+        <h4 className="listItem" > Github: www.github.com/swan1625  </h4>
+        <h4 className="listItem" > LinkedIn: www.linkedin.com/in/andy-swanson-6609b4140/  </h4>
+      </div>
+      </div>
+      
+      </>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,34 +8,39 @@ function Nav() {
   const user = useSelector((store) => store.user);
 
   function myFunction() {
-    var x = document.getElementById("myLinks");
+    let x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
   }
-  
+  function closeFunction() {
+    let x = document.getElementById("myLinks");
+    x.style.display = "none";
+  }
+
   return (
-<>   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+    <>   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
-    <div class="topnav">
-    <a href="#home" class="active"> 🌷 Pic Some Flowers 🌷</a>
-    {/* <!-- Navigation links (hidden by default) --> */}
-    <div id="myLinks">
-      <a href="#flowerlist" >Flower Index</a>
-      <a href="#userlist">Digital Bouquet</a>
-      <a href="#about">About</a>
-      <div>  <LogOutButton className="navLink" /> </div> 
+      <div class="topnav">
+        <a href="#home" class="active"> 🌷 Pic Some Flowers 🌷</a>
+        {/* <!-- Navigation links (hidden by default) --> */}
+        <div id="myLinks">
+          <a href='#user' onClick={closeFunction}> Profile Page </a>
+          <a href="#flowerlist" onClick={closeFunction}>Flower Index</a>
+          <a href="#userlist" onClick={closeFunction}>Digital Bouquet</a>
+          <a href="#about" onClick={closeFunction}>About</a>
 
-    </div>
-    <a href="javascript:void(0);" class="icon" onClick={myFunction}>
-      <i class="fa fa-bars"></i>
-    </a>
 
-  
+        </div>
+        <a href="javascript:void(0);" class="icon" onClick={myFunction}>
+          <i class="fa fa-bars"></i>
+        </a>
 
-    </div>
+
+
+      </div>
     </>
   );
 }
