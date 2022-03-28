1a641e34be60484b89fc27fd7119cc46ff27e228
Andy Swanson <swan1625@icloud.com>
2022-03-27 23:26:29 -0500

    about me page is finished and styled

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -8,27 +8,27 @@ import './AboutPage.css'
 
 function AboutPage() {
   return (<>
-  <div style={{ borderStyle: 'dashed', borderColor: '#a5c2af', marginTop: '35px', backgroundColor: '#e2d3e8'}}>
+  <div style={{ borderStyle: 'dashed', borderColor: '#a5c2af', marginTop: '35px', backgroundColor: '#e2d3e8',}}>
     <div className="about-container">
-      <h2 className="listInfoHeading"> Technologies Used</h2>
-      <h4 className="listItem" > • ReactJS / ReduxJS / Javascript </h4>
-      <h4 className="listItem" > • Node.js  / Express / PostgreSQL  </h4>
-      <h4 className="listItem" > • MaterialUI / Html / CSS </h4>
-      <h4 className="listItem" > • Multer Image Upload / Cloudinary </h4>
+      <p className="listInfoHeading">  <b> Technologies Used </b></p>
+      <p className="listItem" > • ReactJS / ReduxJS / Javascript </p>
+      <p className="listItem" > • Node.js  / Express / PostgreSQL  </p>
+      <p className="listItem" > • MaterialUI / Html / CSS </p>
+      <p className="listItem" > • Multer Image Upload / Cloudinary </p>
     </div>
         <div className="about-container">
-        <h2 className="listInfoHeading"> Thank you</h2>
-        <h4 className="listItem" > • Liz Kerber, Matt Black, Kris Szafranski </h4>
-        <h4 className="listItem" > • Prime Digital Academy </h4>
-        <h4 className="listItem" > • Family and Friends </h4>
-        <h4 className="listItem" > • Adams Cohort </h4>
-        <h4 className="listItem" > • www.minnesotawildflowers.info </h4>
+        <p className="listInfoHeading"> <b>Thank you</b></p>
+        <p className="listItem" > • Liz Kerber, Matt Black, Kris Szafranski </p>
+        <p className="listItem" > • Prime Digital Academy </p>
+        <p className="listItem" > • Family and Friends </p>
+        <p className="listItem" > • Adams Cohort </p>
+        <p className="listItem" > • www.minnesotawildflowers.info </p>
       </div>
       <div className="about-container">
-        <h2 className="listInfoHeading"> Contact Info </h2>
-        <h4 className="listItem" > <b>Email:</b> andyswanson44@gmail.com </h4>
-        <h4 className="listItem" > Github: www.github.com/swan1625  </h4>
-        <h4 className="listItem" > LinkedIn: www.linkedin.com/in/andy-swanson-6609b4140/  </h4>
+        <p className="listInfoHeading"> <b>Contact Info </b></p>
+        <p className="listItem" > <b>Email:</b> andyswanson44@gmail.com </p>
+        <p className="listItem" > <b>Github:</b> www.github.com/swan1625  </p>
+        <p className="listItem" > <b>LinkedIn:</b> www.linkedin.com/in/andy-swanson-6609b4140/  </p>
       </div>
       </div>
       
